343a5c14276bd3987648b9af63055b7a7accdb63
Luiz Fernando Araujo da Silva <luizfernandoaraujodasilva@hotmail.com>
2023-09-15 13:37:19 -0300

    feat: Head

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react"
+import { Separator } from "./components/ui/separator";
 export function App() {
   return (
-    <h1>Hello World</h1>
-  )
+      <div className="min-h-screen flex flex-col ">
+        {/* Header */}
+        <div className="px-6 py-3 flex items-center justify-between border-b">
+          <h1 className="text-xl font-bold">Upload.ai</h1>
+
+          <div className="flex items-center gap-3">
+            <span className="text-sm text-muted-foreground">
+              Desenvolvido com 💙 no NLW da Rocketseat</span>
+            
+            <Separator orientation="vertical" className="h-6"/>
+            
+            <Button variant="outline">
+              <Github className="w-4 h-4 mr-2"/>
+              GitHub
+              </Button>
+          </div>
+
+        </div>
+
+        {/* Main */}
+        <div className="flex-1">
+
+        </div>
+      </div>
+    )
 }
 
